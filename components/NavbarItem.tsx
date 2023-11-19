93f3d5ad16477bd7afb4dec82e821aefa5879b60
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2023-11-19 14:32:38 -0300

    ft: change icons  when dark mode is active

--- components/NavbarItem.tsx
+++ components/NavbarItem.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import Image from 'next/image';
 
 interface NavbarItemProps {
-    label: string
+    label?: string;
+    dark?: boolean
 }
 
 const NavbarItem: React.FC<NavbarItemProps> = ({
-    label
+    label,
+    dark,
 }) => {
     return (
         <div className="text-gray-100 cursor-pointer hover:text-white hover:underline underline-offset-2 transition flex gap-2">
             <Image
             width={30}
             height={5}
-            src='/images/pokeball.png'
+            src={`${dark ? '/images/ultraball.png' : '/images/pokeball.png'}`}
             alt='pokeball'/>
             {label}
         </div>
--- components/NavbarMenu.tsx
+++ components/NavbarMenu.tsx
@@ -3,7 +3,7 @@ import NavbarItem from "./NavbarItem";
 
 interface NavbarMenuProps {
     visible?: boolean;
-    dark: boolean;
+    dark?: boolean;
 }
 
 const NavbarMenu: React.FC<NavbarMenuProps> = ({
@@ -16,11 +16,11 @@ const NavbarMenu: React.FC<NavbarMenuProps> = ({
     }
 
     return (
-        <div className={`${dark ? 'bg-zinc-800 ' : 'bg-red-600'} absolute shadow-md shadow-zinc-600 rounded-xl flex flex-col justify-center top-[75%] right-[11%] gap-4 ml-4 px-8 py-4`}>
-            <NavbarItem label="Home"/>
-            <NavbarItem label="Create"/>
-            <NavbarItem label="My Account"/>
-            <NavbarItem label="About"/>
+        <div className={`${dark ? 'bg-zinc-800 ' : 'bg-red-600'} absolute shadow-md shadow-zinc-600 rounded-xl flex flex-col justify-center top-[75%] right-[11%] gap-4 pl-2 pr-8 py-4`}>
+            <NavbarItem dark={dark} label="Home"/>
+            <NavbarItem dark={dark} label="Create"/>
+            <NavbarItem dark={dark} label="My Account"/>
+            <NavbarItem dark={dark} label="About"/>
         </div>
     )
 };

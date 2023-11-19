8c7a940f620196e62c6d431312a0d0db0c26b000
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2023-11-19 14:50:49 -0300

    ft: add responsive design to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -39,8 +39,14 @@ export default function Navbar () {
                 src="/images/header.png" alt=""/>
             </header>
             <div className="flex gap-1">
-                <button className="text-white rounded-md w-[100px] bg-sky-500 h-[35px]">Search</button>
-                <input className="w-[200px] rounded-md h-[35px] px-2" type="text" placeholder="Search your pokemon ..."/>
+                <button className="text-white rounded-3xl w-[40px] bg-sky-500 h-[40px] flex items-center justify-center hover:saturate-150 hover:scale-110">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-search" width="25" height="25" viewBox="0 0 24 24" stroke-width="2.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+                        <path d="M21 21l-6 -6" />
+                    </svg>
+                </button>
+                <input className="w-[140px] md:w-[200px] rounded-3xl h-[40px] px-3" type="text" placeholder="Search your pokemon ..."/>
             </div>
             <div className="w-30 flex gap-2">
                 <button onClick={toggleNavbarMenu} className="border-2 rounded-full p-2">

c3cb387a6ea7096f59a6631cde79681e96b8ca41
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2024-02-10 17:29:45 -0300

    ft: add ordering

--- components/PokemonList.tsx
+++ components/PokemonList.tsx
@@ -3,12 +3,11 @@ import OrderAndFilter from "./OrderAndFilter";
 import {  useEffect } from "react";
 import { AppState } from "@/redux/store";
 import { getPokemons } from "@/redux/actions";
-import { useAppDispatch  } from "@/redux/hooks";
-import { useSelector } from "react-redux";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 
 const PokemonList = () => {
 
-    const pokemons = useSelector((state?: AppState) => state?.pokemons);
+    const data = useAppSelector((state?: AppState) => state?.pokemons);
     const dispatch = useAppDispatch();
 
     useEffect(() => {
@@ -19,7 +18,7 @@ const PokemonList = () => {
         <main className="flex w-screen h-screen justify-center items-center">
             <div className="grid md:grid-cols-3 lg:grid-cols-4 w-[80%] h-[60%] gap-5 lg:gap-8">
                 {
-                pokemons?.map((pokemon: any) => {
+                data?.map((pokemon: any) => {
                     return (
                         <Card
                         key={pokemon.id}

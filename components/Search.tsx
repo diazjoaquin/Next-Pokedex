bf4dd984bfca548d117fc48540547af0659e2dc9
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2024-02-09 21:10:06 -0300

    ft: add search pokemon by name to searchbar

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,18 +1,39 @@
+import { getPokemonByName } from "@/redux/actions";
+import { useAppDispatch } from "@/redux/hooks";
+import { useState } from "react";
+
+
 export default function Search () {
 
+    const dispatch = useAppDispatch();
+    const [ input, setInput ] = useState('');
+
+    const handleChange = (event : any) => {
+        setInput(event.target.value);
+    }
+
     
+    const handleSubmit = (event: any) => {
+        if (!input) {
+            return alert ('Insert a valid name');
+        }
+        event.preventDefault();
+        dispatch(getPokemonByName(input));
+        
+        setInput('');
+    }
 
 
     return (
         <div className="flex gap-1">
-            <button className="text-white rounded-3xl w-[40px] bg-sky-500 h-[40px] flex items-center justify-center hover:saturate-150 hover:scale-110">
+            <button onClick={(event) => handleSubmit(event)} className="text-white rounded-3xl w-[40px] bg-sky-500 h-[40px] flex items-center justify-center hover:saturate-150 hover:scale-110">
                 <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-search" width="25" height="25" viewBox="0 0 24 24" strokeWidth="2.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
                     <path d="M21 21l-6 -6" />
                 </svg>
             </button>
-            <input className="w-[140px] md:w-[200px] rounded-3xl h-[40px] px-3" type="text" placeholder="Search your pokemon ..."/>
+            <input className="w-[140px] md:w-[200px] rounded-3xl h-[40px] px-3" value={input} type="text" onChange={(event) => handleChange(event)} placeholder="Search your pokemon ..."/>
         </div>
     )
 };

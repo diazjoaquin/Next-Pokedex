7157bc26c9429caa7726b80144f324739f9d7fd9
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2024-02-15 12:06:53 -0300

    add routes to navbar items

--- components/NavbarMenu.tsx
+++ components/NavbarMenu.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import NavbarItem from "./NavbarItem";
+import { useRouter } from "next/router";
 
 interface NavbarMenuProps {
     visible?: boolean;
@@ -11,16 +12,18 @@ const NavbarMenu: React.FC<NavbarMenuProps> = ({
     dark
 }) => {
 
+    const router = useRouter();
+
     if (!visible) {
          return null;
     }
 
     return (
         <div className={`${dark ? 'bg-zinc-800 ' : 'bg-red-600'} absolute shadow-md shadow-zinc-600 rounded-xl flex flex-col justify-center top-[75%] right-[11%] gap-4 pl-2 pr-8 py-4`}>
-            <NavbarItem dark={dark} label="Home"/>
-            <NavbarItem dark={dark} label="Create"/>
-            <NavbarItem dark={dark} label="My Account"/>
-            <NavbarItem dark={dark} label="About"/>
+            <span onClick={() => {router.push('/')}}><NavbarItem dark={dark} label="Home"/></span>
+            <span onClick={() => {router.push('/create')}}><NavbarItem dark={dark} label="Create"/></span>
+            {/* <NavbarItem dark={dark} label="My Account"/> */}
+            <span onClick={() => {router.push('/about')}}><NavbarItem dark={dark} label="About"/></span>
         </div>
     )
 };

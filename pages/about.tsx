5f9805ce1c10fb6dbcf36612dbb558f679439d95
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2024-02-15 12:24:14 -0300

    responsive about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -13,24 +13,28 @@ export default function About () {
             <Navbar/>
             <Sidebar/>
             <section className={`${darkTheme ? 'bg-zinc-700' : 'bg-red-600'} 
-            w-8/12 
-            h-4/6
+            md:w-8/12 
+            md:h-4/6
+            w-11/12
+            h-5/6
             absolute 
-            bottom-20
+            md:bottom-20
             flex
             flex-col
-            gap-2
-            px-12
-            py-10
-            justify-evenly
-            items-center
+            md:gap-2
+            md:px-12
+            px-6
+            md:py-10
+            py-6
+            justify-around
+            
             rounded-2xl
             shadow-zinc-600
             shadow-md`}>
-                <h1 className="text-white font-bold text-2xl">Acerca del Proyecto</h1>
+                <h1 className="text-white font-bold text-lg md:text-2xl">Acerca del Proyecto:</h1>
                 <p className="text-white">Este proyecto surge junto a mi deseo de profundizar el aprendizaje en nuevas tecnologías web. Inicialmente concebido como un proyecto individual, (en el cual utilicé tecnologías como React, Javascript, PostgreSQL, Node.js y Express) durante mi ultimo transcurso en mi carrera como Full Stack Web Developer en Henry , decidí reversionarlo y llevarlo al siguiente nivel al adoptar Next.js, MongoDB y TypeScript, tres tecnologías que han capturado mi interés y que estoy ansioso por dominar.</p>
-                <h3 className="text-white font-bold text-2xl">Tecnologías Usadas en este Proyecto:</h3>
-                <div className="text-white flex flex-wrap gap-6">
+                <h3 className="text-white font-bold text-lg md:text-2xl">Tecnologías Usadas en este Proyecto:</h3>
+                <div className="text-white flex flex-wrap gap-3 md:gap-6">
                     <span>TypeScript</span>
                     <span>TailwindCSS</span>
                     <span>Next.js</span>
@@ -38,7 +42,7 @@ export default function About () {
                     <span>AtlasDB</span>
                     <span>Redux and Redux Toolkit</span>
                 </div>
-                <h3 className="text-white font-bold text-2xl">Estado Actual:</h3>
+                <h3 className="text-white font-bold text-lg md:text-2xl">Estado Actual:</h3>
                 <p className="text-white">Actualmente este proyecto se encuentra en desarrollo. Con la posibilidad de agregar nuevas funciones más adelante. Espero sea de tu agrado!</p>
                 <div className="flex flex-row gap-2 text-white text-4xl">
                 <a href="https://github.com/diazjoaquin" className="hover:scale-125 hover:brightness-150 transition ease-in-out delay-150"><AiFillGithub/></a>

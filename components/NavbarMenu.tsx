ea1404d37ef4bad4ae4709d94f3ed26e176f0c00
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2024-02-22 23:38:03 -0300

    add logout to nav menu

--- components/NavbarMenu.tsx
+++ components/NavbarMenu.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import NavbarItem from "./NavbarItem";
 import { useRouter } from "next/router";
+import { useCallback } from "react";
+import { signOut } from "next-auth/react";
 
 interface NavbarMenuProps {
     visible?: boolean;
@@ -14,6 +16,17 @@ const NavbarMenu: React.FC<NavbarMenuProps> = ({
 
     const router = useRouter();
 
+    const logout = useCallback(async () => {
+        try {
+          await signOut({
+            callbackUrl: '/auth'
+          });
+        } catch (error) {
+          console.log(error);
+          
+        }
+      }, []);
+
     if (!visible) {
          return null;
     }
@@ -22,8 +35,8 @@ const NavbarMenu: React.FC<NavbarMenuProps> = ({
         <div className={`${dark ? 'bg-zinc-800 ' : 'bg-red-600'} absolute shadow-md shadow-zinc-600 rounded-xl flex flex-col justify-center top-[75%] right-[11%] gap-4 pl-2 pr-8 py-4`}>
             <span onClick={() => {router.push('/')}}><NavbarItem dark={dark} label="Home"/></span>
             <span onClick={() => {router.push('/create')}}><NavbarItem dark={dark} label="Create"/></span>
-            {/* <NavbarItem dark={dark} label="My Account"/> */}
             <span onClick={() => {router.push('/about')}}><NavbarItem dark={dark} label="About"/></span>
+            <span onClick={() => logout()}><NavbarItem dark={dark} label="Log Out"/></span>
         </div>
     )
 };

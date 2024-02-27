ca6f29213a6305142145e8eeb13e50da8becda64
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2024-02-26 23:03:24 -0300

    add new functions and dark theme styles

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,30 +1,55 @@
+import React from 'react';
 import { 
     MdKeyboardArrowRight, 
     MdKeyboardArrowLeft, 
     MdKeyboardDoubleArrowLeft, 
     MdKeyboardDoubleArrowRight } 
 from "react-icons/md";
-import React from 'react';
+import { useTheme } from "@/hooks/ThemeContext";
+
 
 interface PaginationProps {
     currentPage: number;
+    numOfPages: number;
     handleNext: () => void;
     handlePrevious: () => void;
+    handleToEnd: () => void;
+    handleToBeginning: () => void;
 }
 
 
-export const Pagination: React.FC<PaginationProps> = ({currentPage, handleNext, handlePrevious}) => {
+export const Pagination: React.FC<PaginationProps> = ({
+    currentPage, 
+    numOfPages,
+    handleNext, 
+    handlePrevious, 
+    handleToBeginning, 
+    handleToEnd}) => {
+
+    const { darkTheme } = useTheme(); 
 
     return (
-        <div className="absolute top-[150px]">
+        <div className="absolute top-[125px] md:top-[150px]">
             <div className="">
                 {
                     <div className="flex gap-2 text-white">
-                        <button className="border-2 rounded-full bg-red-500 px-2 hover:bg-red-600 hover:scale-110"><MdKeyboardDoubleArrowLeft /></button>
-                        <button className="border-2 rounded-full bg-red-500 px-2 hover:bg-red-600 hover:scale-110" onClick={() => handlePrevious()}><MdKeyboardArrowLeft /></button>
-                        <button className="rounded-full font-bold border-2 bg-red-500 text-white px-3 py-1">{currentPage}</button>
-                        <button className="border-2 rounded-full bg-red-500 px-2 hover:bg-red-600 hover:scale-110" onClick={() => handleNext()}><MdKeyboardArrowRight /></button>
-                        <button className="border-2 rounded-full bg-red-500 px-2 hover:bg-red-600 hover:scale-110"><MdKeyboardDoubleArrowRight /></button>
+                        <button className={`${darkTheme ? 'bg-zinc-700 hover:zinc-800' : 'bg-red-500 hover:bg-red-600'} 
+                        border-2 rounded-full bg-red-500 px-2 hover:scale-110 
+                        ${ currentPage === 1 ? 'pointer-events-none' : '' }`} 
+                        onClick={() => handleToBeginning()}><MdKeyboardDoubleArrowLeft /></button>
+                        <button className={`${darkTheme ? 'bg-zinc-700 hover:zinc-800' : 'bg-red-500 hover:bg-red-600'} 
+                        border-2 rounded-full px-2 hover:scale-110 
+                        ${ currentPage === 1 ? 'pointer-events-none' : '' }`} onClick={() => handlePrevious()}><MdKeyboardArrowLeft /></button>
+                        <button className={`${darkTheme ? 'bg-zinc-700 hover:zinc-800' : 'bg-red-500 hover:bg-red-600'} 
+                        rounded-full font-bold text-white px-3 py-1`}>{currentPage}</button>
+                        <button className={`${darkTheme ? 'bg-zinc-700 hover:zinc-800' : 'bg-red-500 hover:bg-red-600'} 
+                        border-2 rounded-full px-2 hover:scale-110 
+                        ${ currentPage === (Math.ceil(numOfPages)) ? 'pointer-events-none' : '' }`} 
+                        onClick={() => handleNext()}><MdKeyboardArrowRight /></button>
+                        <button className={`${darkTheme ? 'bg-zinc-700 hover:zinc-800' : 'bg-red-500 hover:bg-red-600'} 
+                        border-2 rounded-full px-2 hover:scale-110 
+                        ${ currentPage === (Math.ceil(numOfPages)) ? 'pointer-events-none' : '' }`} 
+                        onClick={() => handleToEnd()}><MdKeyboardDoubleArrowRight /></button>
                     </div>
                 }
             </div>

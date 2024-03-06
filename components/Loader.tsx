7ad073c53e034228f6ee4b3ee9fa346f1ed1aa6a
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2024-03-06 20:51:45 -0300

    add new loaders

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,11 +1,19 @@
 import Image from "next/image";
-import Gif from '@/public/images/assets/4OKl.gif';
 
 export default function Loader () {
 
+    const loaders = [
+        '/images/assets/4o45.gif',
+        '/images/assets/4OKl.gif',
+        '/images/assets/2N06.gif',
+        '/images/assets/28ei.gif'
+    ];
+
+    const randomIndex = Math.floor(Math.random() * loaders.length);
+
     return (
         <main className="w-screen h-screen flex items-center justify-center">
-            <Image src={Gif} alt="" width={500} height={500}/>
+            <Image src={loaders[randomIndex]} alt="loader" width={500} height={500}/>
         </main>
     )
 };

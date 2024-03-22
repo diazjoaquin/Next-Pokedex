28b72c07245d80babd63af4bc00e62421e1ff5a2
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2024-03-22 20:54:06 -0300

    add github loggin and update build comand

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useState, useCallback } from "react";  
 import { signIn, getSession } from 'next-auth/react';
 import { FcGoogle } from 'react-icons/fc';
-// import { FaGithub } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
 import Input from '@/components/Input';
 import { NextPageContext } from 'next';
 import { useRouter } from 'next/router';
@@ -106,9 +106,9 @@ const Auth = () => {
                           <div onClick={() => signIn('google', { callbackUrl: '/' })} className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition'>
                             <FcGoogle size={30}/>
                           </div>
-                          {/* <div onClick={() => signIn('github', { callbackUrl: '/' })} className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition'>
+                          <div onClick={() => signIn('github', { callbackUrl: '/' })} className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition'>
                             <FaGithub size={30}/>
-                          </div> */}
+                          </div>
                         </div>
                         <p className="text-neutral-500 mt-12">
                           {variant === 'login' ? 'First time?' : 'Already have an account?'}

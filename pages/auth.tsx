48beeffb3011979c893e1749a7fdb6bef81f5202
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2024-03-22 21:37:58 -0300

    change callbackurl on google and github

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -103,10 +103,10 @@ const Auth = () => {
                           {variant === 'login' ? 'Login' : 'Sign Up'}
                         </button>
                         <div className='flex flex-row items-center gap-4 mt-8 justify-center'>
-                          <div onClick={() => signIn('google', { callbackUrl: '/' })} className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition'>
+                          <div onClick={() => signIn('google', { callbackUrl: 'https://next-pokedex-red.vercel.app/auth/callback/google' })} className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition'>
                             <FcGoogle size={30}/>
                           </div>
-                          <div onClick={() => signIn('github', { callbackUrl: '/' })} className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition'>
+                          <div onClick={() => signIn('github', { callbackUrl: 'https://next-pokedex-red.vercel.app/auth/callback/github' })} className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition'>
                             <FaGithub size={30}/>
                           </div>
                         </div>
